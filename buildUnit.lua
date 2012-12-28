44509f262e28c071cf6bc567bce7e49d2aa74e7a
andy <andy@stormbase.net>
2012-12-28 16:44:42 +0000

    updated unit builder to print unknown elements

--- buildUnit.lua
+++ buildUnit.lua
@@ -1,24 +1,27 @@
 local addon, ns = ...
 local config = ns.config
 
+local elements = ns.elements
 local common = config.elements.common 	--needed once per unit
 
 local applyCommon = function(specific)
 
 	local result = {}
 
-	for k,v in pairs(common) do
-		result[k] = v
+	for k, v in ipairs(common) do
+		table.insert(result, v)
 	end
 
-	for k,v in pairs(specific) do
-		result[k] = v
+	for k,v in ipairs(specific) do
+		table.insert(result, v)
 	end
 	
+	return result
+
 end
 
 
-local buildUnit = function(self, ...)
+local buildUnit = function(self, unit, ...)
 
 	self:RegisterForClicks("AnyUp")
 	self:SetScript("OnEnter", UnitFrame_OnEnter)
@@ -29,7 +32,12 @@ local buildUnit = function(self, ...)
 	local unitConfig = applyCommon(config.elements[unit])
 
 	for i, element in ipairs(unitConfig) do
-		print(i,v)
+
+		if elements[element] then
+			elements[element](self, unit, ...)
+		else
+			print("unkown", element)
+		end
 	end
 
 end
