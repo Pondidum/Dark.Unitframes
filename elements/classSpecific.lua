5b6f8a1ef3ec92b192ce238a7961acb567d5c3d2
andy <andy@stormbase.net>
2013-01-02 01:17:11 +0000

    classSpecific: fix class picker

--- elements/classSpecific.lua
+++ elements/classSpecific.lua
@@ -2,7 +2,9 @@ local addon, ns = ...
 
 local classSpecific = function(self, unit)
 
-	local spec = ns.elements.specific[unit]
+	local class, classFile = UnitClass(unit)
+
+	local spec = ns.elements.specific[string.lower(classFile)]
 
 	if spec then
 		spec(self, unit)
