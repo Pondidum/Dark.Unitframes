5ba7d5cdaf59fee704b8293189c5f2e5b3edf21a
andy <andy@stormbase.net>
2013-01-10 12:53:40 +0000

    classSpecific.shaman: fixed anchoring and sizing

--- elements/classSpecific.shaman.lua
+++ elements/classSpecific.shaman.lua
@@ -2,13 +2,16 @@ local addon, ns = ...
 local core = Dark.core
 local style = core.style
 
+local TOTEM_HEIGHT = 8
+
 local shamanSpecific = function(self, unit)
 
 	local totems = {}
 
 	for i = 1, MAX_TOTEMS do
 
-		local totem = CreateFrame("StatusBar", nil, self)
+		local totem = CreateFrame("StatusBar", "DarkTotem" .. i, self)
+		totem:SetHeight(TOTEM_HEIGHT)
 		totem:SetStatusBarTexture(core.textures.normal)
 		totem:GetStatusBarTexture():SetHorizTile(false)
 
@@ -21,11 +24,23 @@ local shamanSpecific = function(self, unit)
 
 	end
 
-	totems[1]:SetPoint("BOTTOMLEFT", self.Health, "TOPLEFT", 0, 0)
-	for i = 2, MAX_TOTEMS do 
-		totems[i]:SetPoint("LEFT", totems[i - 1], "RIGHT", ns.config.spacing, 0)
-	end
+	--i cant think of a better way of doing this without hardcoding widths, or creating surplus frames
+	self:SetScript("OnSizeChanged", function(self, w, h) 
 
+		local totemWidth = (w - (ns.config.spacing * 3)) / 4
+
+		totems[1]:SetPoint("BOTTOMLEFT", self.Health, "TOPLEFT", 0, ns.config.spacing)
+		totems[1]:SetWidth(totemWidth)
+
+		for i = 2, MAX_TOTEMS do 
+
+			totems[i]:SetPoint("LEFT", totems[i - 1], "RIGHT", ns.config.spacing, 0)
+			totems[i]:SetWidth(totemWidth)
+
+		end
+
+	end)
+	
 	self.DarkTotems = totems
 
 end
