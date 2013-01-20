87fd2e9d795e7210774a9ca456fd4fe180792459
andy <andy@stormbase.net>
2013-01-20 23:11:11 +0000

    classSpecific: druid: added mushroombar

--- elements/classSpecific.druid.lua
+++ elements/classSpecific.druid.lua
@@ -3,21 +3,10 @@ local core = Dark.core
 local style = core.style
 
 local ECLIPSE_HEIGHT = 8
+local MAX_MUSHROOMS = 3
 
--- local eclipseDirection = function(self)
+local createEclipseBar = function(self, unit)
 
--- 	if ( GetEclipseDirection() == "sun" ) then
--- 			self.Text:SetText("|cffE5994C".."Starfire".."|r")
--- 	elseif ( GetEclipseDirection() == "moon" ) then
--- 			self.Text:SetText("|cff4478BC".."Wrath".."|r")
--- 	else
--- 			self.Text:SetText("")
--- 	end
-
--- end
-
-local druidSpecific = function(self, unit)
-	
 	local eclipse = CreateFrame("Frame", nil, self)
 	eclipse:SetPoint("BOTTOMLEFT", self.Health, "TOPLEFT", 0, ns.config.spacing)
 	eclipse:SetPoint("BOTTOMRIGHT", self.Health, "TOPRIGHT", 0, ns.config.spacing)
@@ -41,9 +30,67 @@ local druidSpecific = function(self, unit)
 
 	eclipse.SolarBar = solar
 
+	return eclipse
 
-	self.EclipseBar = eclipse
-	self.classSpecific = eclipse
+end
+
+local createMushrooms = function(self, unit)
+	
+	local mushrooms = CreateFrame("Frame", nil, self)
+
+	mushrooms.colors = {
+		[1] = {0.65, 0.63, 0.35, 0.6},
+		[2] = {0.65, 0.63, 0.35, 0.6},
+		[3] = {0.65, 0.63, 0.35, 0.6},
+	}
+
+	mushrooms:SetPoint("BOTTOMLEFT", self.EclipseBar, "TOPLEFT", 0, ns.config.spacing)
+	mushrooms:SetPoint("BOTTOMRIGHT", self.EclipseBar, "TOPRIGHT", 0, ns.config.spacing)
+	mushrooms:SetHeight(ECLIPSE_HEIGHT)
+
+	for i = 1, MAX_MUSHROOMS do
+
+		local totem = CreateFrame("StatusBar", nil, mushrooms)
+		totem:SetHeight(ECLIPSE_HEIGHT)
+		totem:SetStatusBarTexture(core.textures.normal)
+		totem:GetStatusBarTexture():SetHorizTile(false)
+
+		style.addShadow(totem)
+		style.addBackground(totem)
+
+		totem.bg = totem:CreateTexture(nil, 'BORDER')
+
+		mushrooms[i] = totem
+
+	end
+
+	--i cant think of a better way of doing this without hardcoding widths, or creating surplus frames
+	mushrooms:SetScript("OnSizeChanged", function(self, w, h) 
+
+		local totemWidth = (w - (ns.config.spacing *  (MAX_MUSHROOMS - 1))) / MAX_MUSHROOMS
+
+		mushrooms[1]:SetPoint("BOTTOMLEFT", mushrooms, "BOTTOMLEFT", 0, 0)
+		mushrooms[1]:SetWidth(totemWidth)
+
+		for i = 2, MAX_MUSHROOMS do 
+
+			mushrooms[i]:SetPoint("LEFT", mushrooms[i - 1], "RIGHT", ns.config.spacing, 0)
+			mushrooms[i]:SetWidth(totemWidth)
+
+		end
+
+	end)
+
+	return mushrooms
+
+end
+
+local druidSpecific = function(self, unit)
+	
+	self.EclipseBar = createEclipseBar(self, unit)
+	self.DarkTotems = createMushrooms(self, unit)
+
+	self.classSpecific = self.DarkTotems
 
 end
 
