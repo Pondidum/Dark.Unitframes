5aaf07955952a67753320617573f312c365d9ce9
andy <andy@stormbase.net>
2013-02-02 17:17:59 +0000

    classSpecific: shaman: updated totems to be statusbars

--- elements/classSpecific.shaman.lua
+++ elements/classSpecific.shaman.lua
@@ -9,29 +9,46 @@ ns.elements.specific.shaman = {
 	create = function(self, unit)
 		
 		local totems = CreateFrame("Frame", nil, self)
-		
-		ns.builder.autoSegments("StatusBar", totems, MAX_TOTEMS, ns.config.spacing, function(segment)
 
+		ns.builder.autoSegments("StatusBar", totems, MAX_TOTEMS, ns.config.spacing, function(segment, i)
+
+			segment:SetMinMaxValues(0, 1)
 			segment:SetStatusBarTexture(core.textures.normal)
 			segment:GetStatusBarTexture():SetHorizTile(false)
 
 			style.addShadow(segment)
 			style.addBackground(segment)
-
+			
 			segment:SetHeight(TOTEM_HEIGHT)
 
 		end)
+
+		local colors = self.colors
+
+		totems:SetScript("OnUpdate", function(self)
+
+			for i = 1, MAX_TOTEMS do
+
+				local id = totems[i]:GetID()
+				local haveTotem, name, start, duration, icon = GetTotemInfo(id)
+			
+				totems[i]:SetValue(1 - ((GetTime() - start) / duration))
+				totems[i]:SetStatusBarColor(unpack(colors.totems[id]))
+
+			end
+
+		end)
 		
-		self.DarkTotems = totems
+		self.Totems = totems
 		self.classSpecific = totems
 		
 	end,
 
 	layout = function(self)
 
-		self.DarkTotems:SetPoint("BOTTOMLEFT", self.Health, "TOPLEFT", 0, ns.config.spacing)
-		self.DarkTotems:SetPoint("BOTTOMRIGHT", self.Health, "TOPRIGHT", 0, ns.config.spacing)
-		self.DarkTotems:SetHeight(TOTEM_HEIGHT)
+		self.Totems:SetPoint("BOTTOMLEFT", self.Health, "TOPLEFT", 0, ns.config.spacing)
+		self.Totems:SetPoint("BOTTOMRIGHT", self.Health, "TOPRIGHT", 0, ns.config.spacing)
+		self.Totems:SetHeight(TOTEM_HEIGHT)
 
 	end,
 }
