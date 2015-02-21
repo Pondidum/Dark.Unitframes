11d8c06b51c2acc5505d1a5d6da25ebde78cf0cf
andy <andy@andydote.co.uk>
2015-02-21 12:04:15 +0000

    elements.generic: use ns.lib

--- elements/classSpecific.generic.lua
+++ elements/classSpecific.generic.lua
@@ -1,27 +1,37 @@
 local addon, ns = ...
-local core = Dark.core
-local style = core.style
+
+local style = ns.lib.style
 
 local BAR_HEIGHT = 8
 
 local composition = {
-	
+
 	create = function(self, unit)
 
 		local bars = CreateFrame("Frame", nil, self)
 
-		
+
 		ns.builder.segments("Frame", bars, 5, function(segment)
 
-			style.addShadow(segment)
-			style.addBackground(segment)
+			local border = CreateFrame("Frame", nil, segment)
 
+			border:SetFrameLevel(1)
+			border:SetFrameStrata(segment:GetFrameStrata())
+			border:SetPoint("TOPLEFT", -3, 3)
+			border:SetPoint("BOTTOMLEFT", -3, -3)
+			border:SetPoint("TOPRIGHT", 3, 3)
+			border:SetPoint("BOTTOMRIGHT", 3, -3)
+
+			style:background(segment)
+			style:border(border)
+
+			segment.border = border
 			segment:SetHeight(8)
 
 		end)
 
 		--i cant think of a better way of doing this without hardcoding widths, or creating surplus frames
-		bars.PostUpdate = function(elements, current, max, changed) 
+		bars.PostUpdate = function(elements, current, max, changed)
 
 			local w = elements:GetWidth()
 			local totemWidth = (w - (ns.config.spacing * (max - 1) )) / max
@@ -29,7 +39,7 @@ local composition = {
 			bars[1]:SetPoint("BOTTOMLEFT", bars, "BOTTOMLEFT", 0, 0)
 			bars[1]:SetWidth(totemWidth)
 
-			for i = 2, max do 
+			for i = 2, max do
 
 				bars[i]:SetPoint("LEFT", bars[i - 1], "RIGHT", ns.config.spacing, 0)
 				bars[i]:SetWidth(totemWidth)
@@ -44,14 +54,14 @@ local composition = {
 			local colors = self.colors
 			local class, classFile = UnitClass(unit)
 
-			if not forceCP and colors ~= nil then		
+			if not forceCP and colors ~= nil then
 				red, green, blue = unpack(colors.class[classFile])
 			else
 				red, green, blue = 1, 0.96, 0.41
 			end
 
 			for i = 1, 5  do
-				element[i].bg:SetBackdropColor(red, green, blue)
+				element[i]:SetBackdropColor(red, green, blue)
 			end
 
 		end
