57815ed376f08033d65de646da7d3f032cfa2670
andy <andy@andydote.co.uk>
2015-02-23 22:31:55 +0000

    toc: remove dark.core dependency

--- initialise.lua
+++ initialise.lua
@@ -14,7 +14,6 @@ local boot = function()
 	ns.tags = {}
 
 	ns.name = "DarkUnitFrames"
-	ns.core = Dark.core
 
 	setmetatable(ns.config.colors, { __index = oUF.colors})
 	setmetatable(ns.config.colors.power, { __index = oUF.colors.power })
